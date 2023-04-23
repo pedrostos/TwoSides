dc989fb38c869c31d02e9a0e8e3f31300f891f29
Erick Gomes <lekogomes2013@gmail.com>
2023-04-23 17:07:00 -0300

    Adicionando inteligencia artificial nos inimigos para perseguir o player

--- src/entidades/Inimigo.java
+++ src/entidades/Inimigo.java
@@ -2,10 +2,28 @@ package entidades;
 
 import java.awt.image.BufferedImage;
 
+import game.Game;
+import world.World;
+
 public class Inimigo extends Entidade{
+	
+	private double speed = 1;
 
 	public Inimigo(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 	}
 
+	public void tick() {
+		if(x < Game.player.getX() && World.isFree((int)(x+speed), this.getY())) {
+			x += speed;
+		} else if (x > Game.player.getX() && World.isFree((int)(x-speed), this.getY())) {
+			x -= speed;
+		}
+		if(y < Game.player.getY() && World.isFree(this.getX(),(int)(y+speed))) {
+			y += speed;
+		} else if (y > Game.player.getY() && World.isFree(this.getX(),(int)(y-speed))) {
+			y -= speed;
+		}
+	}
+	
 }
